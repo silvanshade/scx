56acdb9c1f192ee50d314ce02fdbc1368c74369a
Piotr Gorski <lucjan.lucjanov@gmail.com>
2025-07-15 09:12:12 +0200

    scx_loader: Add mitosis scheduler
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- rust/scx_loader/src/config.rs
+++ rust/scx_loader/src/config.rs
@@ -83,6 +83,7 @@ pub fn get_default_config() -> Config {
         SupportedSched::Rustland,
         SupportedSched::Cosmos,
         SupportedSched::WD40,
+        SupportedSched::Mitosis,
     ];
     let scheds_map = HashMap::from(supported_scheds.map(|x| init_default_config_entry(x)));
     Config {
@@ -227,6 +228,8 @@ fn get_default_scx_flags_for_mode(scx_sched: &SupportedSched, sched_mode: SchedM
         SupportedSched::Cosmos => vec![],
         // scx_wd40 doesn't support any of these modes
         SupportedSched::WD40 => vec![],
+        // scx_mitosis doesn't support any of these modes
+        SupportedSched::Mitosis => vec![],
     }
 }
 
@@ -305,6 +308,13 @@ powersave_mode = []
 server_mode = []
 
 [scheds.scx_wd40]
+auto_mode = []
+gaming_mode = []
+lowlatency_mode = []
+powersave_mode = []
+server_mode = []
+
+[scheds.scx_mitosis]
 auto_mode = []
 gaming_mode = []
 lowlatency_mode = []
--- rust/scx_loader/src/lib.rs
+++ rust/scx_loader/src/lib.rs
@@ -38,6 +38,8 @@ pub enum SupportedSched {
     Cosmos,
     #[serde(rename = "scx_wd40")]
     WD40,
+    #[serde(rename = "scx_mitosis")]
+    Mitosis,
 }
 
 impl FromStr for SupportedSched {
@@ -49,6 +51,7 @@ impl FromStr for SupportedSched {
             "scx_cosmos" => Ok(SupportedSched::Cosmos),
             "scx_flash" => Ok(SupportedSched::Flash),
             "scx_lavd" => Ok(SupportedSched::Lavd),
+            "scx_mitosis" => Ok(SupportedSched::Mitosis),
             "scx_p2dq" => Ok(SupportedSched::P2DQ),
             "scx_tickless" => Ok(SupportedSched::Tickless),
             "scx_rustland" => Ok(SupportedSched::Rustland),
@@ -73,6 +76,7 @@ impl From<SupportedSched> for &str {
             SupportedSched::Cosmos => "scx_cosmos",
             SupportedSched::Flash => "scx_flash",
             SupportedSched::Lavd => "scx_lavd",
+            SupportedSched::Mitosis => "scx_mitosis",
             SupportedSched::P2DQ => "scx_p2dq",
             SupportedSched::Tickless => "scx_tickless",
             SupportedSched::Rustland => "scx_rustland",
