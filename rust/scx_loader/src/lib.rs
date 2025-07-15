8ecc2accfec29c0f783186e2d2938f51baa10417
Piotr Gorski <lucjan.lucjanov@gmail.com>
2025-07-15 09:11:52 +0200

    scx_loader: Add wd40 scheduler
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- rust/scx_loader/src/lib.rs
+++ rust/scx_loader/src/lib.rs
@@ -36,6 +36,8 @@ pub enum SupportedSched {
     Rustland,
     #[serde(rename = "scx_cosmos")]
     Cosmos,
+    #[serde(rename = "scx_wd40")]
+    WD40,
 }
 
 impl FromStr for SupportedSched {
@@ -51,6 +53,7 @@ impl FromStr for SupportedSched {
             "scx_tickless" => Ok(SupportedSched::Tickless),
             "scx_rustland" => Ok(SupportedSched::Rustland),
             "scx_rusty" => Ok(SupportedSched::Rusty),
+            "scx_wd40" => Ok(SupportedSched::WD40),
             _ => Err(anyhow::anyhow!("{scx_name} is not supported")),
         }
     }
@@ -74,6 +77,7 @@ impl From<SupportedSched> for &str {
             SupportedSched::Tickless => "scx_tickless",
             SupportedSched::Rustland => "scx_rustland",
             SupportedSched::Rusty => "scx_rusty",
+            SupportedSched::WD40 => "scx_wd40",
         }
     }
 }
--- rust/scx_loader/src/main.rs
+++ rust/scx_loader/src/main.rs
@@ -97,6 +97,7 @@ impl ScxLoader {
             "scx_tickless",
             "scx_rustland",
             "scx_rusty",
+            "scx_wd40",
         ]
     }
 
